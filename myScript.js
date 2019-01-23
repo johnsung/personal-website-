7a80812dfe50e7a0066488481a9dfa79fbd72d95
johnsung <45019484+johnsung@users.noreply.github.com>
2019-01-23 14:47:34 -0800

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,13 @@
+<script>
+var i = 0;
+var txt = 'Lorem ipsum dummy text blabla.';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+</script>
